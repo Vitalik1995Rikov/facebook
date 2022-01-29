e081d4bc368b2c4472fca3d0d01d9d2a1c7587cd
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-29 11:31:11 +0300

    added LeftSide component

---
+++ src/components/LeftSide/LeftSide.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CopyRights from "./CopyRights";
+
+const LeftSide = () => {
+    return (
+        <div>
+            <CopyRights/>
+        </div>
+    )
+}
+
+export default LeftSide
