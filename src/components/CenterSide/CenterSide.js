14060538fa8ff9fcbf726467964c8e5f1b4e2916
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-01-29 16:38:41 +0300

    added Post, AddPost components

--- src/components/CenterSide/CenterSide.js
+++ src/components/CenterSide/CenterSide.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Stories from "./Stories";
+import Post from "./Post";
+import AddPost from "./AddPost";
 
 const CenterSide = () => {
     return (
         <div className="m-2 shadow-lg h-screen">
             <Stories/>
+            <AddPost/>
+            <Post/>
         </div>
     )
 }
